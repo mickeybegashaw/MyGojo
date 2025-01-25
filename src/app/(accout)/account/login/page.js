93d6916b2ffaeb77e5137a1dbfef47f5209c85f0
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-25 20:26:07 +0300

    Implement login and registration forms with session checks, add loading component, and enhance user navigation

--- src/app/(accout)/account/login/page.js
+++ src/app/(accout)/account/login/page.js
@@ -2,10 +2,11 @@ import SignInButton from "@/app/components/SignInButton";
 import { auth } from "../../../../../auth";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 const Login = async () => {
-  const session =await  auth()
+  const session = await auth();
   if (session) {
-    redirect('/')
+    redirect("/");
   }
   return (
     <div className="py-10 md:py-0 flex items-center flex-col-reverse md:flex-row ">
@@ -13,11 +14,33 @@ const Login = async () => {
         <h1 className="text-xl  text-slate-700 text-center">
           Login to your Account
         </h1>
-          <SignInButton />
+        <form className="flex flex-col w-8/12 gap-4 my-5">
+          <input
+            type="text"
+            placeholder="Email"
+            className="input p-2 border border-gray-300"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input p-2 border border-gray-300"
+          />
+        </form>
+        <SignInButton />
+        <Link href="/account/register">
+          <p className="text-slate-600 mt-5 cursor-pointer hover:underline">
+            Don't have account?
+          </p>
+        </Link>
       </div>
       <div className="flex flex-col w-full items-center">
         <h1 className="text-3xl   text-slate-500">Welcome back</h1>
-        <Image src={'/images/logo.png'} width={400} height={300} alt="logo of myGojo"/>
+        <Image
+          src={"/images/logo.png"}
+          width={400}
+          height={300}
+          alt="logo of myGojo"
+        />
       </div>
     </div>
   );
--- src/app/(accout)/account/register/page.js
+++ src/app/(accout)/account/register/page.js
@@ -1,10 +1,58 @@
-
-const Register = () => {
+import SignInButton from "@/app/components/SignInButton";
+import { auth } from "../../../../../auth";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+const Register = async () => {
+  const session = await auth();
+  if (session) {
+    redirect("/");
+  }
   return (
-    <div>
-      <h1>REgiter</h1>
+    <div className="py-10 md:py-0 flex items-center flex-col-reverse md:flex-row-reverse ">
+      <div className=" w-11/12 h-96 md:w-full md:h-screen bg-white flex flex-col justify-center items-center">
+        <h1 className="text-xl md:text-2xl  text-slate-700 text-center">
+          Create an Account
+        </h1>
+        <form className="flex flex-col w-1/2 gap-4 my-10">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="input p-2 border border-gray-300"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="input p-2 border border-gray-300"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="input p-2 border border-gray-300"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input p-2 border border-gray-300"
+          />
+        </form>
+        <SignInButton />
+        <Link href="/account/login">
+          <p className="text-slate-600 mt-5 cursor-pointer hover:underline">
+            Already have account?
+          </p>
+        </Link>
+      </div>
+      <div className="flex flex-col w-6/12 items-center">
+        <Image
+          src={"/images/logo.png"}
+          width={400}
+          height={300}
+          alt="logo of myGojo"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
